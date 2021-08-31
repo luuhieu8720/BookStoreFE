aee57e44e42d57dc6fb2660ca1c13fd05782acd9
luuhieu8720 <lethiluuhieu@gmail.com>
2021-08-31 21:53:44 +0700

    fix comments

--- src/services/categories.services.ts
+++ src/services/categories.services.ts
@@ -1,21 +1,20 @@
 import {api} from "@/boot/axios";
-import authHeader from "@/models/user/auths.header";
 
 export default new class {
     private readonly category = 'categories';
     getCategory(id: number){
-        return api.get(this.category + "/" + id.toString())
+        return api.get(`${this.category}${"/"}${id.toString()}`)
     }
     getCategories(){
         return api.get(this.category);
     }
     getBookCategory(categoryId: number){
-        return api.get(this.category + "/" + categoryId.toString() + "/books")
+        return api.get(`${this.category}${"/"}${categoryId.toString()}${"/books"}`)
     }
     deleteCategory(id: number){
-        return api.delete(this.category + "/" + id.toString(), { headers: authHeader() });
+        return api.delete(`${this.category}${"/"}${id.toString()}`);
     }
     updateCategory(id: number, category: unknown){
-        return api.put(this.category + "/" + id.toString(), category, { headers: authHeader() })
+        return api.put(`${this.category}${"/"}${id.toString()}`, category)
     }
 }
