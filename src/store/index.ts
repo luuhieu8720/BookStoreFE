64e6e25edc71c4faf86184eefc0004ed2d591dae
luuhieu8720 <lethiluuhieu@gmail.com>
2021-08-31 15:21:41 +0700

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,7 @@
 import { createStore } from 'vuex'
 import {UserDetail} from "@/models/user/user.detail";
-import BookItem from "@/models/book/books"
 
 export const TOKEN = 'TOKEN'
-export const BOOK = 'BOOK'
-export const SET_BOOK = 'SET_BOOK'
 export const SET_TOKEN = 'SET_TOKEN'
 export const CLEAR_TOKEN = 'CLEAR_TOKEN'
 export const SET_USER = 'SET_USER'
@@ -12,14 +9,12 @@ export const SET_USER = 'SET_USER'
 export interface StoreState {
   token: string;
   user: UserDetail | null;
-  book: BookItem | null
 }
 
 function createDefaultState(): StoreState {
   return {
     token: localStorage.getItem(TOKEN) || '',
     user: null,
-    book: null,
   }
 }
 
@@ -37,17 +32,14 @@ export default createStore({
       localStorage.setItem(TOKEN, token)
     },
     [SET_USER] (state: StoreState, user: UserDetail) {
+
       state.user = user
     },
-    [SET_BOOK] (state: StoreState, book: BookItem){
-      state.book = book
-    }
   },
   actions: {
     [CLEAR_TOKEN]: ({ commit }) => commit(CLEAR_TOKEN),
     [SET_TOKEN]: ({ commit }, token: string) => commit(SET_TOKEN, token),
     [SET_USER]: async ({ commit }, user: UserDetail) => commit(SET_USER, user),
-    [SET_USER]: async ({ commit }, book: BookItem) => commit(SET_BOOK, book),
   },
   modules: {
   }
