48a7e8a55cb0697ac4e647f7941b8af851f104bf
luuhieu8720 <lethiluuhieu@gmail.com>
2021-08-31 15:28:33 +0700

    align code

--- src/components/Author/EditAuthor.vue
+++ src/components/Author/EditAuthor.vue
@@ -1,31 +1,31 @@
 <template>
-        <div class="container" :style="{marginLeft:'30px'}">
-			<form>
-				<div class="row">
-					<div class="col-md-8 col-md-offset-2">
-						<h3  id="padding-page">Chỉnh sửa tác giả</h3>
-                            <div class="form-group">
-                                    <label for="name">Mã tác giả<span class="require">*</span></label>
-                                   <input type = "text" name="id" :placeholder="author.id" v-model="author.id" id="padding-bottom-border" class="form-control" readonly />
-                            </div>
-                             <div class="form-group">
-								<label for="name">Tên tác giả<span class="require">*</span></label>
-								<input type = "text" name="name" :placeholder="author.name" v-model="author.name" id="padding-bottom-border" class="form-control" />
-							</div>
+	<div class="container" :style="{marginLeft:'30px'}">
+		<form>
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<h3  id="padding-page">Chỉnh sửa tác giả</h3>
+						<div class="form-group">
+								<label for="name">Mã tác giả<span class="require">*</span></label>
+								<input type = "text" name="id" :placeholder="author.id" v-model="author.id" id="padding-bottom-border" class="form-control" readonly />
+						</div>
 							<div class="form-group">
-								<label for="birthday">Ngày sinh<span class="require">*</span></label>
-								<input type="date" name="description"  v-model="author.birthday" id="padding-bottom-border" class="form-control" />
-							</div>
-							<div class="form-group">
-								<input type = "button" class="btn btn-primary" target="__blank" value="Cập nhật"
-								:style="{borderRadius: '10px'}" @click="updateAuthor()" />
-								<input type = "button" class="btn default" target="__blank" value="Hủy" @click="goBack()" 
-								:style="{marginLeft:'10px', border: '1px solid black', borderRadius:'10px'}" />
-							</div> 
-                    </div>
-                </div>
-			</form>
-		</div>
+							<label for="name">Tên tác giả<span class="require">*</span></label>
+							<input type = "text" name="name" :placeholder="author.name" v-model="author.name" id="padding-bottom-border" class="form-control" />
+						</div>
+						<div class="form-group">
+							<label for="birthday">Ngày sinh<span class="require">*</span></label>
+							<input type="date" name="description"  v-model="author.birthday" id="padding-bottom-border" class="form-control" />
+						</div>
+						<div class="form-group">
+							<input type = "button" class="btn btn-primary" target="__blank" value="Cập nhật"
+							:style="{borderRadius: '10px'}" @click="updateAuthor()" />
+							<input type = "button" class="btn default" target="__blank" value="Hủy" @click="goBack()" 
+							:style="{marginLeft:'10px', border: '1px solid black', borderRadius:'10px'}" />
+						</div> 
+				</div>
+			</div>
+		</form>
+	</div>
 
 </template>
 <script>
