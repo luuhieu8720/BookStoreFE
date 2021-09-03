8fe32300aae3a7ace537bbf853d08abacf7cf8c7
luuhieu8720 <lethiluuhieu@gmail.com>
2021-09-03 09:26:39 +0700

    Refactoring GetBooks

--- src/components/Books/GetBooks.vue
+++ src/components/Books/GetBooks.vue
@@ -1,27 +1,27 @@
-<template>     
-    <div class="p-m-2">
-      <Card>
-        <template #title>
-          Kho sách
-        </template>
-        <template #content>
-          <DataTable :value="books" :loading="isLoading">
-            <Column field="id" header="Id"/>
-            <Column field="name" header="Tên sách"/>
-            <Column field="author_name" header="Tác giả"/>
-            <Column field="price" header="Giá"/>
-            <Column field="year" header="Year"/>
-            <Column>
-              <template #body="slotProps">
-                <router-link :to="{name: 'GetBookById', params: {id: slotProps.data.id}}">
-                  <Button class="btn btn-success" label='Chi tiết'/>
-                </router-link>
-              </template>
-            </Column>
-          </DataTable>
-        </template>
-      </Card>
-    </div>  
+<template>
+  <div class="p-m-2">
+    <Card>
+      <template #title> Kho sách </template>
+      <template #content>
+        <DataTable :value="books" :loading="isLoading">
+          <Column field="id" header="Id" />
+          <Column field="name" header="Tên sách" />
+          <Column field="author_name" header="Tác giả" />
+          <Column field="price" header="Giá" />
+          <Column field="year" header="Year" />
+          <Column>
+            <template #body="slotProps">
+              <router-link
+                :to="{ name: 'GetBookById', params: { id: slotProps.data.id } }"
+              >
+                <Button class="btn btn-success" label="Chi tiết" />
+              </router-link>
+            </template>
+          </Column>
+        </DataTable>
+      </template>
+    </Card>
+  </div>
 </template>  
 
 <script lang="ts">
@@ -30,23 +30,25 @@ import { defineComponent, onMounted, ref } from "vue";
 import BookItem from "@/models/book/books";
 
 export default defineComponent({
-  setup(){
+  setup() {
     const books = ref([] as BookItem[]);
     const isLoading = ref(false);
 
     onMounted(() => {
       isLoading.value = true;
-      books.value = BookServices.getBooks().then(response => {
-        books.value = response.data;
-      }).finally(() => {
-        isLoading.value = false;
-      })
+      BookServices.getBooks()
+        .then((response) => {
+          books.value = response.data;
+        })
+        .finally(() => {
+          isLoading.value = false;
+        });
     });
 
     return {
       books,
-      isLoading
+      isLoading,
     };
-  }
+  },
 });
 </script>
